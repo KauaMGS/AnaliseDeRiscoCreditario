09aacc561fac128068c53a85da43e552e1c273a3
Kauã <kauamoreira90@gmail.com>
2024-08-26 22:09:37 -0300

    Criacao model cliente

---
+++ src/main/java/com/kgsm/AnaliseDeRiscoCreditario/model/Cliente.java
@@ -0,0 +1,32 @@
+package com.kgsm.AnaliseDeRiscoCreditario.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String cpf;
+	
+	@Column(nullable = false)
+	private Double rendaMensal;
+	
+	@Column
+	private String endereco;
+	
+	
+	
+}
