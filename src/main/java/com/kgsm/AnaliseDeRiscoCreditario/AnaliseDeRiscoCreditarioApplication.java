037c007f62d5cafde294141bf4145e7939474bd0
Kauã <kauamoreira90@gmail.com>
2024-08-26 21:13:28 -0300

    Commit inicial

---
+++ src/main/java/com/kgsm/AnaliseDeRiscoCreditario/AnaliseDeRiscoCreditarioApplication.java
@@ -0,0 +1,13 @@
+package com.kgsm.AnaliseDeRiscoCreditario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseDeRiscoCreditarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseDeRiscoCreditarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kgsm/AnaliseDeRiscoCreditario/AnaliseDeRiscoCreditarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgsm.AnaliseDeRiscoCreditario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliseDeRiscoCreditarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
